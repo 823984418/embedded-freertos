c7fbe46e3cbbb16fb552f7f23e3c244c81b5a111
ZQDQ <823984418@qq.com>
2026-06-27 07:58:38 +0800

    更改

--- build.rs
+++ build.rs
@@ -2,43 +2,32 @@ use std::ffi::OsString;
 
 const EMBEDDED_FREERTOS_INCLUDE: &str = "EMBEDDED_FREERTOS_INCLUDE";
 
-#[allow(non_camel_case_types)]
-pub enum Portable {
-    ARM_CM3,
-    ARM_CM4F,
-    Template,
-}
-
-impl Portable {
-    pub fn from_env() -> Self {
-        let target = build_rs::input::target();
-        match target.as_str() {
-            "thumbv7m-none-eabi" => Self::ARM_CM3,
-            "thumbv7em-none-eabi" => Self::ARM_CM3,
-            "thumbv7em-none-eabihf" => Self::ARM_CM4F,
-            _ => {
-                build_rs::output::warning(&format!(
-                    "Unknow target {target}, use template portable!"
-                ));
-                Self::Template
-            }
-        }
-    }
-
-    pub fn path(&self) -> &'static str {
-        match self {
-            Portable::ARM_CM3 => "FreeRTOS-Kernel/portable/GCC/ARM_CM3",
-            Portable::ARM_CM4F => "FreeRTOS-Kernel/portable/GCC/ARM_CM4F",
-            Portable::Template => "FreeRTOS-Kernel/portable/template",
-        }
-    }
-}
-
 fn main() {
     let target = build_rs::input::target();
-    let target_feature = build_rs::input::cargo_cfg_target_feature();
     let out_dir = build_rs::input::out_dir();
     let features = build_rs::input::cargo_cfg_feature();
+
+    let mut portable = features
+        .iter()
+        .filter(|i| i.starts_with("portable-"))
+        .collect::<Vec<_>>();
+    portable.sort();
+    if portable.len() > 1 {
+        panic!(
+            "Enable at most one heap at the same time, enabled: {:?}",
+            portable
+        );
+    }
+    let portable = portable.first();
+    let portable = match portable.map(|i| i.as_str()) {
+        Some("portable-MSVC-MingW") => "FreeRTOS-Kernel/portable/MSVC-MingW",
+        Some("portable-ARM_CM0") => "FreeRTOS-Kernel/portable/GCC/ARM_CM0",
+        Some("portable-ARM_CM3") => "FreeRTOS-Kernel/portable/GCC/ARM_CM3",
+        Some("portable-ARM_CM4F") => "FreeRTOS-Kernel/portable/GCC/ARM_CM4F",
+        _ => "FreeRTOS-Kernel/portable/template",
+    };
+    build_rs::output::rerun_if_changed(portable);
+
     let mut heap = features
         .iter()
         .filter(|i| i.starts_with("heap_"))
@@ -51,8 +40,10 @@ fn main() {
         );
     }
     let heap = heap.first();
-
-    let portable = Portable::from_env();
+    let heap = heap.map(|i| format!("FreeRTOS-Kernel/portable/MemMang/{}.c", i));
+    if let Some(heap) = &heap {
+        build_rs::output::rerun_if_changed(heap);
+    }
 
     build_rs::output::rerun_if_env_changed(EMBEDDED_FREERTOS_INCLUDE);
     let include = std::env::var_os(EMBEDDED_FREERTOS_INCLUDE).unwrap_or(OsString::from("include"));
@@ -69,12 +60,9 @@ fn main() {
 
     bindgen = bindgen.clang_args(["-isystem", "stdlib"]);
     bindgen = bindgen.clang_arg(format!("--target={target}"));
-    for i in &target_feature {
-        bindgen = bindgen.clang_args(["-Xclang", "-target-feature", "-Xclang", &format!("+{i}")]);
-    }
 
     bindgen = bindgen.clang_args(["-I", &include.to_string_lossy()]);
-    bindgen = bindgen.clang_args(["-I", portable.path()]);
+    bindgen = bindgen.clang_args(["-I", portable]);
     bindgen = bindgen.clang_args(["-I", "FreeRTOS-Kernel/include"]);
 
     bindgen = bindgen.header("FreeRTOS-Kernel/include/FreeRTOS.h");
@@ -92,12 +80,9 @@ fn main() {
 
     cc.flags(["-isystem", "stdlib"]);
     cc.flag(format!("--target={target}"));
-    for i in &target_feature {
-        cc.flags(["-Xclang", "-target-feature", "-Xclang", &format!("+{i}")]);
-    }
 
     cc.include(include);
-    cc.include(portable.path());
+    cc.include(portable);
     cc.include("FreeRTOS-Kernel/include");
 
     cc.file("FreeRTOS-Kernel/croutine.c");
@@ -108,8 +93,17 @@ fn main() {
     cc.file("FreeRTOS-Kernel/tasks.c");
     cc.file("FreeRTOS-Kernel/timers.c");
 
+    for i in std::fs::read_dir(portable).unwrap() {
+        if let Ok(entry) = i {
+            let path = entry.path();
+            if path.is_file() && (path.ends_with(".c") || path.ends_with(".s")) {
+                cc.file(entry.path());
+            }
+        }
+    }
+
     if let Some(heap) = heap {
-        cc.file(format!("FreeRTOS-Kernel/portable/MemMang/{}.c", heap));
+        cc.file(heap);
     }
 
     cc.compile("embedded-freertos");
