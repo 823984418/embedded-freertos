4c74b2d8885886104156ffd67b8609c96fbde52e
ZQDQ <823984418@qq.com>
2026-06-27 15:12:55 +0800

    更改

--- src/lib.rs
+++ src/lib.rs
@@ -142,17 +142,116 @@ pub const queueQUEUE_TYPE_BINARY_SEMAPHORE: u8 = 3;
 pub const queueQUEUE_TYPE_RECURSIVE_MUTEX: u8 = 4;
 pub const queueQUEUE_TYPE_SET: u8 = 5;
 
-// TODO: xQueueCreate
-// TODO: xQueueCreateStatic
-// TODO: xQueueGetStaticBuffers
-// TODO: xQueueSendToFront
-// TODO: xQueueSendToBack
-// TODO: xQueueSend
-// TODO: xQueueOverwrite
-// TODO: xQueueSendToFrontFromISR
-// TODO: xQueueSendToBackFromISR
-// TODO: xQueueOverwriteFromISR
-// TODO: xQueueSendFromISR
+#[cfg(configSUPPORT_DYNAMIC_ALLOCATION = "1")]
+pub unsafe fn xQueueCreate(uxQueueLength: UBaseType_t, uxItemSize: UBaseType_t) -> QueueHandle_t {
+    xQueueGenericCreate(uxQueueLength, uxItemSize, queueQUEUE_TYPE_BASE)
+}
+
+#[cfg(configSUPPORT_STATIC_ALLOCATION = "1")]
+pub unsafe fn xQueueCreateStatic(
+    uxQueueLength: UBaseType_t,
+    uxItemSize: UBaseType_t,
+    pucQueueStorage: *mut u8,
+    pxQueueBuffer: *mut StaticQueue_t,
+) -> QueueHandle_t {
+    xQueueGenericCreateStatic(
+        uxQueueLength,
+        uxItemSize,
+        pucQueueStorage,
+        pxQueueBuffer,
+        queueQUEUE_TYPE_BASE,
+    )
+}
+
+#[cfg(configSUPPORT_STATIC_ALLOCATION = "1")]
+pub unsafe fn xQueueGetStaticBuffers(
+    xQueue: QueueHandle_t,
+    ppucQueueStorage: *mut *mut u8,
+    ppxStaticQueue: *mut *mut StaticQueue_t,
+) -> BaseType_t {
+    xQueueGenericGetStaticBuffers(xQueue, ppucQueueStorage, ppxStaticQueue)
+}
+
+pub unsafe fn xQueueSendToFront(
+    xQueue: QueueHandle_t,
+    pvItemToQueue: *const c_void,
+    xTicksToWait: TickType_t,
+) -> BaseType_t {
+    xQueueGenericSend(xQueue, pvItemToQueue, xTicksToWait, queueSEND_TO_FRONT)
+}
+
+pub unsafe fn xQueueSendToBack(
+    xQueue: QueueHandle_t,
+    pvItemToQueue: *const c_void,
+    xTicksToWait: TickType_t,
+) -> BaseType_t {
+    xQueueGenericSend(xQueue, pvItemToQueue, xTicksToWait, queueSEND_TO_BACK)
+}
+
+pub unsafe fn xQueueSend(
+    xQueue: QueueHandle_t,
+    pvItemToQueue: *const c_void,
+    xTicksToWait: TickType_t,
+) -> BaseType_t {
+    xQueueGenericSend(xQueue, pvItemToQueue, xTicksToWait, queueSEND_TO_BACK)
+}
+
+pub unsafe fn xQueueOverwrite(xQueue: QueueHandle_t, pvItemToQueue: *const c_void) -> BaseType_t {
+    xQueueGenericSend(xQueue, pvItemToQueue, 0, queueOVERWRITE)
+}
+
+pub unsafe fn xQueueSendToFrontFromISR(
+    xQueue: QueueHandle_t,
+    pvItemToQueue: *const c_void,
+    pxHigherPriorityTaskWoken: *mut BaseType_t,
+) -> BaseType_t {
+    xQueueGenericSendFromISR(
+        xQueue,
+        pvItemToQueue,
+        pxHigherPriorityTaskWoken,
+        queueSEND_TO_FRONT,
+    )
+}
+
+pub unsafe fn xQueueSendToBackFromISR(
+    xQueue: QueueHandle_t,
+    pvItemToQueue: *const c_void,
+    pxHigherPriorityTaskWoken: *mut BaseType_t,
+) -> BaseType_t {
+    xQueueGenericSendFromISR(
+        xQueue,
+        pvItemToQueue,
+        pxHigherPriorityTaskWoken,
+        queueSEND_TO_BACK,
+    )
+}
+
+pub unsafe fn xQueueOverwriteFromISR(
+    xQueue: QueueHandle_t,
+    pvItemToQueue: *const c_void,
+    pxHigherPriorityTaskWoken: *mut BaseType_t,
+) -> BaseType_t {
+    xQueueGenericSendFromISR(
+        xQueue,
+        pvItemToQueue,
+        pxHigherPriorityTaskWoken,
+        queueOVERWRITE,
+    )
+}
+
+pub unsafe fn xQueueSendFromISR(
+    xQueue: QueueHandle_t,
+    pvItemToQueue: *const c_void,
+    pxHigherPriorityTaskWoken: *mut BaseType_t,
+) -> BaseType_t {
+    xQueueGenericSendFromISR(
+        xQueue,
+        pvItemToQueue,
+        pxHigherPriorityTaskWoken,
+        queueSEND_TO_BACK,
+    )
+}
+
 // TODO: xQueueCRSendFromISR
 // TODO: xQueueCRReceiveFromISR
 // TODO: xQueueCRSend
@@ -200,7 +299,7 @@ pub unsafe fn xSemaphoreCreateBinary() -> SemaphoreHandle_t {
     )
 }
 
-#[cfg(configSUPPORT_DYNAMIC_ALLOCATION = "1")]
+#[cfg(configSUPPORT_STATIC_ALLOCATION = "1")]
 pub unsafe fn xSemaphoreCreateBinaryStatic(
     pxStaticSemaphore: *mut StaticSemaphore_t,
 ) -> SemaphoreHandle_t {
